1a45ecfc6aa856536590deb018b812a0e70b4496
rndall <96964503+rndall@users.noreply.github.com>
2025-06-13 16:29:34 +0800

    feat: create CV header HTML elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
 const App = () => {
-  return <div>App</div>
+  return (
+    <div>
+      <header>
+        <div>
+          <img
+            src="https://imgs.search.brave.com/b2vg4Pf_xrOQ1A54MDUenluUnF6BQVPD8pS9BiHDTGk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWdz/LnNlYXJjaC5icmF2/ZS5jb20vZUNyUTBa/U1F1eURBaV9pWWxZ/aW1IMlUxQ0xkR3Nq/V2FrOTFBY1RQZm9S/US9yczpmaXQ6NTAw/OjA6MDowL2c6Y2Uv/YUhSMGNITTZMeTlw/Yldkei9Mbk5sWVhK/amFDNWljbUYyL1pT/NWpiMjB2UlVkMVZq/WjEvTFRRMldqWjBZ/bUZhVUZoNS9NMmwz/UVhOUWJVaHJVV3d6/L1RWWXRPRmx0YXpK/RlgxRnUvYnk5eWN6/cG1hWFE2TlRZdy9P/ak15TURveE9qQXZa/enBqL1pTOWhTRkl3/WTBoTk5reDUvT1hK/alJ6bDNMMk5IYkhW/YS9lVFZxWWpJd2Rs/cEhPV292L1pGY3hi/R0p1VW5wTWVsVXov/VEhwTmRpOU5hbU14/VEhwSi9NVTFFU1hs/TmFURkhMMkZYL1dq/QmxVekZIWVZkYU1H/VlQvTVVRdlUwVkdU/MUpWZUUxUy9VekZP/VkRBNVR5OU1WMFl3/L1RGVlJkRkZXWkVK/VmExSlUvTDB4VVNY/ZE5hbFYwV2tjNS9h/bVJYTVd3dlltNVNl/a3hVL1RXOU5VMnQx/WkRKV2FTOWovUkRr/eVVGZEplRTlYVFhn/LmpwZWc"
+            alt=""
+          />
+        </div>
+        <div>
+          <h1>Chanelle Moon</h1>
+          <p>KPOP Idol</p>
+        </div>
+      </header>
+    </div>
+  )
 }
 
 export default App
