98affe937af6a3924b78b42e6be229ea9744b7cd
rndall <96964503+rndall@users.noreply.github.com>
2025-06-17 13:37:52 +0800

    feat: style practical experience section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div className="min-h-dvh py-10">
+    <div className="min-h-dvh py-6">
       <div className="mx-auto grid max-w-3xl gap-12 shadow-2xl">
         <header className="mt-14">
           <section className="bg-200 flex h-44 items-center">
@@ -22,7 +22,7 @@ const App = () => {
           </section>
         </header>
 
-        <main className="grid gap-10 px-4">
+        <main className="grid gap-8 px-4 pb-6">
           <section className="grid gap-2">
             <h2 className="font-title text-lg font-bold uppercase">
               Education
@@ -39,34 +39,40 @@ const App = () => {
             </div>
           </section>
 
-          <section>
-            <h2>Work Experience</h2>
+          <section className="grid gap-2">
+            <h2 className="font-title text-lg font-bold uppercase">
+              Work Experience
+            </h2>
+            <hr className="text-400" />
+            <div className="grid gap-4">
+              <div>
+                <p className="text-300 font-bold">Aug 2024 - present</p>
+                <p className="text-300">FIFTY FIFTY (ATTRAKT)</p>
+                <h3>Main Vocalist & Rapper</h3>
+                <p className="text-sm">
+                  Debuted Aug 30, 2024 with pre-release single Starry Night
+                  followed by mini-album Love Tune
+                </p>
+              </div>
 
-            <div>
-              <p>Aug 2024 - present</p>
-              <p>FIFTY FIFTY (ATTRAKT)</p>
-              <h3>Main Vocalist & Rapper</h3>
-              <p>
-                Debuted Aug 30, 2024 with pre-release single Starry Night
-                followed by mini-album Love Tune
-              </p>
-            </div>
+              <div>
+                <p className="text-300 font-bold">Jun 1 - Dec 31, 2023</p>
+                <p className="text-300">BELIFT LAB</p>
+                <h3>Trainee</h3>
+                <p className="text-sm">
+                  Contestant on JTBC's R U Next?; eliminated in Episode 9,
+                  placed 11th
+                </p>
+              </div>
 
-            <div>
-              <p>Jun 1 - Dec 31, 2023</p>
-              <p>BELIFT LAB</p>
-              <h3>Trainee</h3>
-              <p>
-                Contestant on JTBC's R U Next?; eliminated in Episode 9, placed
-                11th
-              </p>
-            </div>
-
-            <div>
-              <p>Jan 2021 - Dec 2022</p>
-              <p>YueHua Entertainment</p>
-              <h3>Trainee</h3>
-              <p>2 years of full vocal, dance, language training</p>
+              <div>
+                <p className="text-300 font-bold">Jan 2021 - Dec 2022</p>
+                <p className="text-300">YueHua Entertainment</p>
+                <h3>Trainee</h3>
+                <p className="text-sm">
+                  2 years of full vocal, dance, language training
+                </p>
+              </div>
             </div>
           </section>
         </main>
