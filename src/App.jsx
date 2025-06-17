379a3e59abd51c9ab3acee1a4b74713374b6ffe6
rndall <96964503+rndall@users.noreply.github.com>
2025-06-17 12:00:55 +0800

    feat: style header

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,70 @@
 const App = () => {
   return (
-    <div>
-      <header>
-        <div>
-          <img
-            src="https://imgs.search.brave.com/b2vg4Pf_xrOQ1A54MDUenluUnF6BQVPD8pS9BiHDTGk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWdz/LnNlYXJjaC5icmF2/ZS5jb20vZUNyUTBa/U1F1eURBaV9pWWxZ/aW1IMlUxQ0xkR3Nq/V2FrOTFBY1RQZm9S/US9yczpmaXQ6NTAw/OjA6MDowL2c6Y2Uv/YUhSMGNITTZMeTlw/Yldkei9Mbk5sWVhK/amFDNWljbUYyL1pT/NWpiMjB2UlVkMVZq/WjEvTFRRMldqWjBZ/bUZhVUZoNS9NMmwz/UVhOUWJVaHJVV3d6/L1RWWXRPRmx0YXpK/RlgxRnUvYnk5eWN6/cG1hWFE2TlRZdy9P/ak15TURveE9qQXZa/enBqL1pTOWhTRkl3/WTBoTk5reDUvT1hK/alJ6bDNMMk5IYkhW/YS9lVFZxWWpJd2Rs/cEhPV292L1pGY3hi/R0p1VW5wTWVsVXov/VEhwTmRpOU5hbU14/VEhwSi9NVTFFU1hs/TmFURkhMMkZYL1dq/QmxVekZIWVZkYU1H/VlQvTVVRdlUwVkdU/MUpWZUUxUy9VekZP/VkRBNVR5OU1WMFl3/L1RGVlJkRkZXWkVK/VmExSlUvTDB4VVNY/ZE5hbFYwV2tjNS9h/bVJYTVd3dlltNVNl/a3hVL1RXOU5VMnQx/WkRKV2FTOWovUkRr/eVVGZEplRTlYVFhn/LmpwZWc"
-            alt=""
-          />
-        </div>
+    <div className="min-h-dvh">
+      <div className="mx-auto my-10 max-w-3xl shadow-2xl">
+        <header className="mt-14">
+          <section className="bg-200 flex h-44 items-center">
+            <div className="flex size-60 items-center justify-center rounded-tr-full rounded-br-full bg-white">
+              <img
+                className="size-48 rounded-full object-cover"
+                src="https://imgs.search.brave.com/b2vg4Pf_xrOQ1A54MDUenluUnF6BQVPD8pS9BiHDTGk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWdz/LnNlYXJjaC5icmF2/ZS5jb20vZUNyUTBa/U1F1eURBaV9pWWxZ/aW1IMlUxQ0xkR3Nq/V2FrOTFBY1RQZm9S/US9yczpmaXQ6NTAw/OjA6MDowL2c6Y2Uv/YUhSMGNITTZMeTlw/Yldkei9Mbk5sWVhK/amFDNWljbUYyL1pT/NWpiMjB2UlVkMVZq/WjEvTFRRMldqWjBZ/bUZhVUZoNS9NMmwz/UVhOUWJVaHJVV3d6/L1RWWXRPRmx0YXpK/RlgxRnUvYnk5eWN6/cG1hWFE2TlRZdy9P/ak15TURveE9qQXZa/enBqL1pTOWhTRkl3/WTBoTk5reDUvT1hK/alJ6bDNMMk5IYkhW/YS9lVFZxWWpJd2Rs/cEhPV292L1pGY3hi/R0p1VW5wTWVsVXov/VEhwTmRpOU5hbU14/VEhwSi9NVTFFU1hs/TmFURkhMMkZYL1dq/QmxVekZIWVZkYU1H/VlQvTVVRdlUwVkdU/MUpWZUUxUy9VekZP/VkRBNVR5OU1WMFl3/L1RGVlJkRkZXWkVK/VmExSlUvTDB4VVNY/ZE5hbFYwV2tjNS9h/bVJYTVd3dlltNVNl/a3hVL1RXOU5VMnQx/WkRKV2FTOWovUkRr/eVVGZEplRTlYVFhn/LmpwZWc"
+                alt=""
+              />
+            </div>
 
-        <section>
-          <h1>Chanelle Moon</h1>
-          <p>chanelle.moon@email.com</p>
-          <p>+1 (310) 555-0123</p>
-        </section>
-      </header>
+            <div className="font-title grid gap-2.5 ps-16 pt-2 font-bold">
+              <h1 className="text-3xl uppercase">Chanelle Moon</h1>
+              <div>
+                <p>chanelle.moon@email.com</p>
+                <p>+1 (310) 555-0123</p>
+              </div>
+            </div>
+          </section>
+        </header>
 
-      <main>
-        <section>
-          <h2>Education</h2>
+        <main>
+          <section>
+            <h2>Education</h2>
 
-          <div>
-            <h3>BSc in Bioengineering</h3>
-            <p>Yonsei University - Global Leaders College</p>
-            <p>2022 - 2024</p>
-          </div>
-        </section>
+            <div>
+              <h3>BSc in Bioengineering</h3>
+              <p>Yonsei University - Global Leaders College</p>
+              <p>2022 - 2024</p>
+            </div>
+          </section>
 
-        <section>
-          <h2>Work Experience</h2>
+          <section>
+            <h2>Work Experience</h2>
 
-          <div>
-            <p>Aug 2024 - present</p>
-            <p>FIFTY FIFTY (ATTRAKT)</p>
-            <h3>Main Vocalist & Rapper</h3>
-            <p>
-              Debuted Aug 30, 2024 with pre-release single Starry Night followed
-              by mini-album Love Tune
-            </p>
-          </div>
+            <div>
+              <p>Aug 2024 - present</p>
+              <p>FIFTY FIFTY (ATTRAKT)</p>
+              <h3>Main Vocalist & Rapper</h3>
+              <p>
+                Debuted Aug 30, 2024 with pre-release single Starry Night
+                followed by mini-album Love Tune
+              </p>
+            </div>
 
-          <div>
-            <p>Jun 1 - Dec 31, 2023</p>
-            <p>BELIFT LAB</p>
-            <h3>Trainee</h3>
-            <p>
-              Contestant on JTBC's R U Next?; eliminated in Episode 9, placed
-              11th
-            </p>
-          </div>
+            <div>
+              <p>Jun 1 - Dec 31, 2023</p>
+              <p>BELIFT LAB</p>
+              <h3>Trainee</h3>
+              <p>
+                Contestant on JTBC's R U Next?; eliminated in Episode 9, placed
+                11th
+              </p>
+            </div>
 
-          <div>
-            <p>Jan 2021 - Dec 2022</p>
-            <p>YueHua Entertainment</p>
-            <h3>Trainee</h3>
-            <p>2 years of full vocal, dance, language training</p>
-          </div>
-        </section>
-      </main>
+            <div>
+              <p>Jan 2021 - Dec 2022</p>
+              <p>YueHua Entertainment</p>
+              <h3>Trainee</h3>
+              <p>2 years of full vocal, dance, language training</p>
+            </div>
+          </section>
+        </main>
+      </div>
     </div>
   )
 }
