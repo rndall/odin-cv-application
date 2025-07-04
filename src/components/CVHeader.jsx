3220630eed2ecefed257e8afe117ef53e987e6ca
rndall <96964503+rndall@users.noreply.github.com>
2025-07-04 16:52:11 +0800

    feat: implement profile picture uploading

--- src/components/CVHeader.jsx
+++ src/components/CVHeader.jsx
@@ -7,10 +7,17 @@ const CVHeader = () => {
   const [editMode, setEditMode] = useState(false)
 
   const [generalInfo, setGeneralInfo] = useState({
+    profilePicture: null,
     name: "",
     email: "",
     phoneNumber: "",
   })
+
+  const imageChange = (e) =>
+    setGeneralInfo({
+      ...generalInfo,
+      profilePicture: URL.createObjectURL(e.target.files[0]),
+    })
 
   const handleEditInfo = (e) =>
     setGeneralInfo({ ...generalInfo, [e.target.id]: e.target.value })
@@ -37,11 +44,38 @@ const CVHeader = () => {
         )}
 
         <div className="flex size-60 items-center justify-center rounded-tr-full rounded-br-full bg-white">
-          <img
-            className="size-48 rounded-full object-cover"
-            src="https://imgs.search.brave.com/b2vg4Pf_xrOQ1A54MDUenluUnF6BQVPD8pS9BiHDTGk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWdz/LnNlYXJjaC5icmF2/ZS5jb20vZUNyUTBa/U1F1eURBaV9pWWxZ/aW1IMlUxQ0xkR3Nq/V2FrOTFBY1RQZm9S/US9yczpmaXQ6NTAw/OjA6MDowL2c6Y2Uv/YUhSMGNITTZMeTlw/Yldkei9Mbk5sWVhK/amFDNWljbUYyL1pT/NWpiMjB2UlVkMVZq/WjEvTFRRMldqWjBZ/bUZhVUZoNS9NMmwz/UVhOUWJVaHJVV3d6/L1RWWXRPRmx0YXpK/RlgxRnUvYnk5eWN6/cG1hWFE2TlRZdy9P/ak15TURveE9qQXZa/enBqL1pTOWhTRkl3/WTBoTk5reDUvT1hK/alJ6bDNMMk5IYkhW/YS9lVFZxWWpJd2Rs/cEhPV292L1pGY3hi/R0p1VW5wTWVsVXov/VEhwTmRpOU5hbU14/VEhwSi9NVTFFU1hs/TmFURkhMMkZYL1dq/QmxVekZIWVZkYU1H/VlQvTVVRdlUwVkdU/MUpWZUUxUy9VekZP/VkRBNVR5OU1WMFl3/L1RGVlJkRkZXWkVK/VmExSlUvTDB4VVNY/ZE5hbFYwV2tjNS9h/bVJYTVd3dlltNVNl/a3hVL1RXOU5VMnQx/WkRKV2FTOWovUkRr/eVVGZEplRTlYVFhn/LmpwZWc"
-            alt=""
-          />
+          {generalInfo.profilePicture ? (
+            <div className="group relative">
+              <label className="absolute top-1/2 left-1/2 mx-auto flex -translate-1/2 items-center rounded-full bg-blue-500 px-5 py-1 text-white opacity-0 transition group-hover:opacity-100 hover:bg-blue-600">
+                Upload
+                <input
+                  type="file"
+                  name="profilePicture"
+                  id="profilePicture"
+                  accept="image/*"
+                  onChange={imageChange}
+                  hidden
+                />
+              </label>
+              <img
+                className="size-48 rounded-full object-cover"
+                src={generalInfo.profilePicture}
+                alt="Profile picture"
+              />
+            </div>
+          ) : (
+            <label className="flex items-center rounded-full bg-blue-500 px-5 py-1 text-white transition hover:bg-blue-600">
+              Upload
+              <input
+                type="file"
+                name="profilePicture"
+                id="profilePicture"
+                accept="image/*"
+                onChange={imageChange}
+                hidden
+              />
+            </label>
+          )}
         </div>
 
         {editMode ? (
